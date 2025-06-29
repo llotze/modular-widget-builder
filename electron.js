90e247aee0f38ff19d05b9b7f4fae76f36373682
llotze <llotze@bu.edu>
2025-06-29 18:29:00 -0400

    Initial commit

---
+++ electron.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron');
+const http = require('http');
+
+function waitForServer(url, cb) {
+  const tryConnect = () => {
+    http.get(url, () => cb()).on('error', () => setTimeout(tryConnect, 500));
+  };
+  tryConnect();
+}
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  waitForServer('http://localhost:3000', () => {
+    win.loadURL('http://localhost:3000');
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Your Widgets</h1>
+      {/* List widgets and "Create Widget" button */}
+    </div>
+  );
+}
